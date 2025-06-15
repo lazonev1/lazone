dfbca4b4c7b90f17dcbc7e5e29e180b0f64b4256
moisedk <moisedete@gmail.com>
2025-06-15 04:52:27 -0400

    Add options button to main message screen

--- apps/mobile/app/(tabs)/messages.tsx
+++ apps/mobile/app/(tabs)/messages.tsx
@@ -1,14 +1,26 @@
 import { SafeAreaView, StyleSheet } from 'react-native';
 import { ThemedView } from '@/components/ThemedView';
-import { ThemedText } from '@/components/ThemedText';
 import MessageList from '../messages/messages';
-import AppHeader from '@/components/ui/AppHeader';
-
+import { TouchableOpacity } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import { View } from 'react-native';
+import { useNavigation } from 'expo-router';
+import { useEffect } from 'react';
 export default function Messages() {
+      const navigation = useNavigation()
+  
+      useEffect(() => {
+          navigation.setOptions({ title: 'Messages' });
+      }, []);
   return (
     <SafeAreaView style={styles.safeArea}>
       <ThemedView style={styles.container}>
-        <AppHeader/>
+        {/* This is decorative  for now. Will be useful for additional features/actions user can take at the main message screen... */}
+        <View style={styles.options}>
+            <TouchableOpacity style={styles.infoButton}>
+            <Ionicons name="ellipsis-horizontal" size={24} color="#fff" />
+            </TouchableOpacity>
+        </View>
         <MessageList/>
       </ThemedView>
     </SafeAreaView>
@@ -25,5 +37,13 @@ const styles = StyleSheet.create({
   safeArea: {
     flex: 1,
     backgroundColor: '#171617',
-  }
+  }, 
+  infoButton: {
+    padding: 8
+  },
+   options: {
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    width: '100%'
+   }
 });
